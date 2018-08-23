52aaddd9e0b5d119009825fd5be48d148610b75a
NguyenKimDien <trietnk01@gmail.com>
2018-08-23 18:14:58 +0700

    cập nhật

--- document/chonviec.sql
+++ document/chonviec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2018 at 11:56 AM
+-- Generation Time: Aug 23, 2018 at 01:12 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.1.17
 
